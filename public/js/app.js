c569f17a9d9e83df9726124298ef1b0bee00031c
belousov <da_belousov@list.ru>
2021-12-26 12:13:43 +0300

    Установка шаблона главной страницы

--- public/js/app.js
+++ public/js/app.js
@@ -5301,6 +5301,57 @@ Vue.component('example-component', (__webpack_require__(/*! ./components/Example
 var app = new Vue({
   el: '#app'
 });
+/*!
+* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  // Navbar shrink function
+  var navbarShrink = function navbarShrink() {
+    var navbarCollapsible = document.body.querySelector('#mainNav');
+
+    if (!navbarCollapsible) {
+      return;
+    }
+
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink');
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink');
+    }
+  }; // Shrink the navbar
+
+
+  navbarShrink(); // Shrink the navbar when page is scrolled
+
+  document.addEventListener('scroll', navbarShrink); // Activate Bootstrap scrollspy on the main nav element
+
+  var mainNav = document.body.querySelector('#mainNav');
+
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      offset: 74
+    });
+  }
+
+  ; // Collapse responsive navbar when toggler is visible
+
+  var navbarToggler = document.body.querySelector('.navbar-toggler');
+  var responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', function () {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
+});
 
 /***/ }),
 
