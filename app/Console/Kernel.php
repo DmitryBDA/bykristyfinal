b06b05055670dfe4f9622b5a0001a6c6165225cc
Dmitry <da_belousov@list.ru>
2022-01-12 17:19:18 +0300

    Отправка уведомлений о необходимости записаться

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,12 +10,16 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('send:notificationTomorrowRecord')
+            ->timezone('Asia/Irkutsk')
+            ->everyMinute();
+
+        $schedule->command('send:notificationNeedRecord')
             ->timezone('Asia/Irkutsk')
             ->everyMinute();
     }
@@ -27,7 +31,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Notifications\NotificationNeedRecord;
 use App\Notifications\NotificationNewRecord;
 use App\Notifications\NotificationTomorrowRecord;
 use App\Presenters\Record\RecordPresenter;
@@ -9,18 +10,20 @@ use Illuminate\Support\Facades\Notification;
 
 class TelegramService
 {
-  public function sendNotificationNewRecord($user, $record){
-      $data = [
-          'name' => $user->name,
-          'phone' => $user->phone,
-          'time' => (new RecordPresenter($record))->time(),
-          'date' => (new RecordPresenter($record))->startDate(),
-      ];
-      Notification::route('telegram', config('config_telegram.TELEGRAM_ADMIN_ID'))
-          ->notify(new NotificationNewRecord($data));
-  }
+    public function sendNotificationNewRecord($user, $record)
+    {
+        $data = [
+            'name' => $user->name,
+            'phone' => $user->phone,
+            'time' => (new RecordPresenter($record))->time(),
+            'date' => (new RecordPresenter($record))->startDate(),
+        ];
+        Notification::route('telegram', config('config_telegram.TELEGRAM_ADMIN_ID'))
+            ->notify(new NotificationNewRecord($data));
+    }
 
-    public function sendNotificationTomorrowRecord($user, $record){
+    public function sendNotificationTomorrowRecord($user, $record)
+    {
         $data = [
             'name' => $user->name,
             'phone' => $user->phone,
@@ -29,5 +32,15 @@ class TelegramService
         ];
         Notification::route('telegram', config('config_telegram.TELEGRAM_ADMIN_ID'))
             ->notify(new NotificationTomorrowRecord($data));
+    }
+
+    public function sendNotificationNeedRecord($user)
+    {
+        $data = [
+            'name' => $user->name,
+            'phone' => $user->phone,
+        ];
+        Notification::route('telegram', config('config_telegram.TELEGRAM_ADMIN_ID'))
+            ->notify(new NotificationNeedRecord($data));
     }
 }
